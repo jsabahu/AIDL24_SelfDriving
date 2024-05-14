2bd809fe05b8e4edb8c5972bc1516ad771ea1391
MarcRamonMoreno <marc.ramon12@outlook.com>
2024-05-14 15:11:31 +0200

    Correction

--- src/model2.py
+++ src/model2.py
@@ -5,8 +5,10 @@ import torchvision.models as models
 from torchvision.ops import MultiScaleRoIAlign, RoIPool
 
 class LaneVehicleDetectionNet(nn.Module):
-    def __init__(self, num_classes):
+    def __init__(self, num_classes, num_anchors=9):
         super(LaneVehicleDetectionNet, self).__init__()
+        
+        self.num_anchors = num_anchors
         
         # Backbone network
         backbone = models.resnet50(pretrained=True)
@@ -23,8 +25,8 @@ class LaneVehicleDetectionNet(nn.Module):
         # Region Proposal Network (RPN)
         self.rpn = nn.ModuleList([
             nn.Conv2d(256, 256, 3, padding=1),
-            nn.Conv2d(256, num_anchors * 2, 1),  # classification
-            nn.Conv2d(256, num_anchors * 4, 1),  # bbox regression
+            nn.Conv2d(256, self.num_anchors * 2, 1),  # classification
+            nn.Conv2d(256, self.num_anchors * 4, 1),  # bbox regression
         ])
         
         # RoI Align
@@ -53,3 +55,30 @@ class LaneVehicleDetectionNet(nn.Module):
             nn.ReLU(),
             nn.Conv2d(64, 1, 1)  # single channel output for binary segmentation
         )
+
+    def forward(self, images, targets=None):
+        # Backbone
+        features = self.backbone(images)
+        
+        # FPN
+        fpn_features = [fpn_layer(features[i]) for i, fpn_layer in enumerate(self.fpn)]
+        
+        # RPN
+        rpn_logits, rpn_bbox = [], []
+        for rpn_layer in self.rpn:
+            rpn_feat = rpn_layer(fpn_features)
+            rpn_logits.append(rpn_feat[:, :self.num_anchors * 2, :, :])
+            rpn_bbox.append(rpn_feat[:, self.num_anchors * 4:, :, :])
+        
+        # RoI Align
+        rois = self.roi_align(fpn_features, targets)
+        
+        # Detection Head
+        detection_features = rois.view(rois.size(0), -1)
+        class_logits = self.classifier(detection_features)
+        bbox_preds = self.bbox_regressor(detection_features)
+        
+        # Lane Detection Head
+        lane_preds = self.lane_head(fpn_features[0])
+        
+        return class_logits, bbox_preds, lane_preds
