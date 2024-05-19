11906f2a55719abe4645883a90bfbcc03cfcc1ee
ginesabata <ginesabata@gmail.com>
2024-05-19 15:53:07 +0200

    Added the function 'binary_accuracy_with_logits' to the 'utils' file, which is used within the trainin

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 import torch
 
+# Function use in train
 def binary_accuracy_with_logits(labels, outputs):
     preds = torch.sigmoid(outputs).round()
     acc = (preds == labels.view_as(preds)).float().detach().numpy().mean()
