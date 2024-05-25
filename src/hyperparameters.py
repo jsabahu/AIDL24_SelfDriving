1d532da05a2b1366ff02ab037ae8e69dcf246718
Fermin Gomila <fermin.gomila@lan.tekniatest.com>
2024-05-25 18:31:03 +0200

    Training working

--- src/hyperparameters.py
+++ src/hyperparameters.py
@@ -1,6 +1,5 @@
 # Define some hyper-parameters using in train
 hparams = {
-    'batch_size': 64,
     'num_epochs': 10,
     'learning_rate': 0.001,
     'weight_decay': 0.0001,
---
+++ src/modelDebug.py
@@ -0,0 +1,40 @@
+import torch.nn as nn
+import torch.nn.functional as F
+ 
+class SimpleSegmentationModel(nn.Module):
+    def __init__(self, input_channels=3, output_channels=1):
+        super(SimpleSegmentationModel, self).__init__()
+       
+        # Encoder
+        self.encoder = nn.Sequential(
+            nn.Conv2d(input_channels, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True)
+        )
+
+        # Decoder
+        self.decoder = nn.Sequential(
+            nn.Conv2d(256, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(32, output_channels, kernel_size=3, padding=1),
+            nn.Sigmoid()  # Ensuring output is between 0 and 1
+        )
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
+ 
