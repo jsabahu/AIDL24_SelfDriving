aea5c69973c2d6c714750baa37aef7a082b92e40
jsabahu <jordisabates1996@hotmail.com>
2024-05-26 23:57:53 +0200

    [JSH] 20240526 improved train code

--- src/train.py
+++ src/train.py
@@ -1,128 +1,178 @@
 import torch
 from torch.utils.data import DataLoader
-import torch.nn.functional as F  # If required not use this black box, can create binary cross entropy in "utils"
-import torch.optim as optim  # If required not use this black box, can create optimizer in "utils"
+import torch.nn.functional as F
+import torch.optim as optim
 import numpy as np
 from torcheval.metrics.functional import binary_accuracy
 from utils import read_yaml
 from dataloader import MyDataset
 from hyperparameters import hparams
-from utils import binary_accuracy_with_logits
+from utils import binary_accuracy_with_logits, save_model
 from model_ENet import ENet
 
 # from model_ENet_complete import ENet
 from torchvision import transforms
 from logger import Logger
 from torch.utils.tensorboard import SummaryWriter
+import os
 
+# Initialize tensorboard writer and logger
 writer = SummaryWriter()
-
 logger = Logger()
 
-device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-# Parameters to use
-config_path = "configs/config.yaml"
-config = read_yaml(config_path)
+# Define device
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+logger.log_debug(f"Using device: {device}")
 
-# Train model one epoch
+# Read configuration
+config_path = "configs/config.yaml"
+try:
+    config = read_yaml(config_path)
+    logger.log_debug(f"Read configuration from {config_path}")
+except Exception as e:
+    logger.log_error(f"Failed to read configuration from {config_path}: {e}")
+    raise
+
+
 def train_single_epoch(model, train_loader, optimizer):
-    model.train()  # Model in training mode
-    accs, losses = [], []  # Init accuracies and losses
+    """
+    Train the model for a single epoch.
+    """
+
+    logger.log_debug("Starting training for a single epoch")
+    model.train()  # Set model to training mode
+    accs, losses = [], []  # Initialize accuracies and losses
+
     for step, (images, masks) in enumerate(train_loader):
+        logger.log_debug(f"Processing step {step}")
         # Move data to device
         images, masks = images.to(device), masks.to(device)
-        # Set network gradients to 0.
-        optimizer.zero_grad()  # Restart gradients
-        # Forward batch of images through the network
+        optimizer.zero_grad()  # Zero the gradients
+
+        # Forward pass
         detection_output, lane_output = model(images)
-        # Adjust size masks
-        masks = masks.unsqueeze(1).float()
+        masks = masks.unsqueeze(1).float()  # Adjust size masks
+
         # Compute loss
         loss = F.binary_cross_entropy_with_logits(
             detection_output, masks
-        ) + F.binary_cross_entropy_with_logits(
-            lane_output, masks
-        )  # Calculate loss
-        # Backward pass: compute gradients of the loss with respect to model parameters
+        ) + F.binary_cross_entropy_with_logits(lane_output, masks)
+
+        # Backward pass and update
         loss.backward()
-        # Update parameters of the network
         optimizer.step()
+
         # Compute metrics
-        acc = binary_accuracy_with_logits(
-            masks, detection_output
-        )  # Not clear! Function using like "Lab 3" from projects
-        # Add loss to list
+        acc = binary_accuracy_with_logits(masks, detection_output)
         losses.append(loss.item())
-        # Add accuracy to list
         accs.append(acc.item())
-    # Add loss and accuracy mean
+
     return np.mean(losses), np.mean(accs)
 
 
+def train_single_epoch_lane_model(model, train_loader, optimizer):
+    """
+    Train the lane detection model for a single epoch.
+    """
+    logger.log_debug("Starting training single epoch (Lane Model)")
+
+    model.train()  # Set model to training mode
+    accs, losses = [], []  # Initialize accuracies and losses
+
+    for step, (images, masks) in enumerate(train_loader):
+        images, masks = images.to(device), masks.to(device)
+        optimizer.zero_grad()  # Zero the gradients
+
+        # Forward pass
+        lane_output = model(images.float())
+        masks = masks.float()
+
+        # Compute loss
+        loss = F.binary_cross_entropy_with_logits(lane_output, masks)
+
+        # Backward pass and update
+        loss.backward()
+        optimizer.step()
+
+        # Flatten the output and target tensors
+        lane_output_flat = lane_output.view(-1)
+        masks_flat = masks.view(-1)
+        acc = binary_accuracy(lane_output_flat, masks_flat, threshold=0.9)
+
+        logger.log_debug(
+            f"Step {step}/{len(train_loader)}: loss={loss.item()}, acc={acc.item()}"
+        )
+        losses.append(loss.item())
+        accs.append(acc.item())
+
+    mean_loss = np.mean(losses)
+    mean_acc = np.mean(accs)
+    logger.log_debug(
+        f"Epoch completed with mean loss: {mean_loss}, mean accuracy: {mean_acc}"
+    )
+
+    return mean_loss, mean_acc
+
+
 def train_model(hparams):
+    """
+    Train the model based on the provided hyperparameters.
+    """
+    logger.log_debug("Starting model training")
+
+    # Define transformation
     transform = transforms.Compose(
         [
             transforms.Resize((256, 256), antialias=True),
             transforms.ToTensor(),
-            # transforms.Normalize(mean=[0.5, 0.5],std=[0.5, 0.5])  # Resize to [3, 256, 256]
+            # transforms.Normalize(mean=[0.5, 0.5],std=[0.5, 0.5]),
         ]
     )
-    # Generate Training dataset
+
+    # Create training dataset and DataLoader
     train_dataset = MyDataset(
         images_path=config["train"]["train_images_path"],
         mask_path=config["train"]["train_labels_path"],
         transform=transform,
-    )  # ->REQUIRED UPDATE PATH<-
-    # Generate DataLoader
+    )
     train_loader = DataLoader(
         train_dataset, batch_size=hparams["batch_size"], shuffle=True
     )
-    # First trials, just one class: Line road??
-    model = ENet(num_classes=1).to(device)  # Just one class: "LINE ROAD"?<-
-    # Initialize optimizer (possibility to create specific optimizer call from utils)
+
+    # Initialize model
+    model = ENet(num_classes=1).to(device)
+
+    # Initialize optimizer
     # Self-drive examples found use the Adam optimizer
     optimizer = optim.Adam(
         model.parameters(),
         lr=hparams["learning_rate"],
         weight_decay=hparams["weight_decay"],
-    )  # Note use weight_decay to prevent overfitting
+    )
+
     # Initialize best_loss
     best_loss = float("inf")  # Inicialitzar best_loss
 
     for epoch in range(hparams["num_epochs"]):
-        # Train model for 1 epoch
-        train_loss, train_acc = train_single_epoch_laneModel(
+        logger.log_debug(f"Starting epoch {epoch}")
+
+        # Train for one epoch
+        train_loss, train_acc = train_single_epoch_lane_model(
             model, train_loader, optimizer
         )
+
         logger.log_debug(
             f"Train Epoch {epoch} loss={train_loss:.2f} acc={train_acc:.2f}"
         )
         writer.add_scalar("Loss/train", train_loss, epoch)
         writer.add_scalar("Acc/train", train_acc, epoch)
-        # Save best lost
+
+        # Save best model
         if train_loss < best_loss:
             best_loss = train_loss
-            torch.save(model.state_dict(), "best_model.pth")
+            save_model(model, config["train"]["model_name"])
             logger.log_debug(f"Saved best model with loss {best_loss:.2f}")
+
+    writer.flush()
+    logger.log_debug("Training completed")
     return model
-
-
-def train_single_epoch_laneModel(model, train_loader, optimizer):
-    logger.log_debug("Starting training single epoch")
-    model.train()
-    accs, losses = [], []
-    for step, (images, masks) in enumerate(train_loader):
-        images, masks = images.to(device), masks.to(device)
-        optimizer.zero_grad()
-        lane_output = model(images.float())
-        masks = masks.float()
-        loss = F.cross_entropy(lane_output, masks)
-        loss.backward()
-        optimizer.step()
-        acc = binary_accuracy(lane_output, masks, threshold=0.9)
-        losses.append(loss.item())
-        accs.append(acc.item())
-    return np.mean(losses), np.mean(accs)
-
-
-writer.flush()
