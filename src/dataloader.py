0b368dc1ee6dec5bbdc4fc8f5565dc0ba75ed119
jsabahu <jordisabates1996@hotmail.com>
2024-06-15 00:57:35 +0200

    [JSH] 20240614 add TuSimple Dataset and example of use

--- src/dataloader.py
+++ src/dataloader.py
@@ -7,19 +7,21 @@ from utils import read_yaml
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
-
+import cv2
+from utils import Rescale
 import random
 
 CONFIG_PATH = "configs/config.yaml"
 
 
 class MaskDataset(Dataset):
-    def __init__(self, images_path, mask_path, transform=None):
+    def __init__(self, images_path, mask_path, transform=None, target_transform=None):
         super().__init__()
         self._root_folder = pathlib.Path(__file__).parent.parent
         self.images_path = os.path.join(self._root_folder, images_path)
         self.mask_path = os.path.join(self._root_folder, mask_path)
         self.transform = transform
+        self.target_transform = target_transform
 
         self.image_files = sorted(
             [f for f in os.listdir(self.images_path) if f.lower().endswith(".jpg")]
@@ -55,7 +57,8 @@ class MaskDataset(Dataset):
 
         if self.transform:
             image = self.transform(image)
-            mask = self.transform(
+        if self.target_transform:
+            mask = self.target_transform(
                 mask
             )  # Es correcte aplicar totes les transformacions a la mask?????
 
@@ -64,41 +67,93 @@ class MaskDataset(Dataset):
         return image, mask
 
 
-if __name__ == "__main__":
-    config = read_yaml(CONFIG_PATH)
+class TusimpleSet(Dataset):
+    def __init__(self, dataset, n_labels=3, transform=None, target_transform=None):
+        self._gt_img_list = []
+        self._gt_label_binary_list = []
+        self._gt_label_instance_list = []
+        self.transform = transform
+        self.target_transform = target_transform
+        self.n_labels = n_labels
 
-    my_transforms = transforms.Compose(
-        [
-            transforms.Resize(tuple(config["resolution"]["360p"]), antialias=True),
-            transforms.ToTensor(),
-        ]
-    )
+        with open(dataset, "r") as file:
+            for _info in file:
+                info_tmp = _info.strip(" ").split()
 
-    dataset = MaskDataset(
-        images_path=config["train"]["train_images_path"],
-        mask_path=config["train"]["train_labels_path"],
-        transform=my_transforms,
-    )
-    dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
+                self._gt_img_list.append(info_tmp[0])
+                self._gt_label_binary_list.append(info_tmp[1])
+                self._gt_label_instance_list.append(info_tmp[2])
 
-    data_iter = iter(dataloader)
-    images, masks = next(data_iter)
+        assert (
+            len(self._gt_img_list)
+            == len(self._gt_label_binary_list)
+            == len(self._gt_label_instance_list)
+        )
 
-    image = images[0]
-    mask = masks[0]
+        self._shuffle()
 
-    print("Image Tensor Type:", type(image))
-    print("Image Tensor Shape:", image.shape)
+    def _shuffle(self):
+        # randomly shuffle all list identically
+        c = list(
+            zip(
+                self._gt_img_list,
+                self._gt_label_binary_list,
+                self._gt_label_instance_list,
+            )
+        )
+        random.shuffle(c)
+        (
+            self._gt_img_list,
+            self._gt_label_binary_list,
+            self._gt_label_instance_list,
+        ) = zip(*c)
 
-    print("Mask Tensor Type:", type(mask))
-    print("Mask Tensor Shape:", mask.shape)
+    def __len__(self):
+        return len(self._gt_img_list)
 
-    # Convert the tensors to numpy arrays for plotting
-    image_np = image.numpy().transpose(1, 2, 0)  # Convert to HWC format for plotting
+    def __getitem__(self, idx):
+        assert (
+            len(self._gt_label_binary_list)
+            == len(self._gt_label_instance_list)
+            == len(self._gt_img_list)
+        )
+
+        # load all
+        img = Image.open(self._gt_img_list[idx])
+        label_instance_img = cv2.imread(
+            self._gt_label_instance_list[idx], cv2.IMREAD_UNCHANGED
+        )
+        label_img = cv2.imread(self._gt_label_binary_list[idx], cv2.IMREAD_COLOR)
+
+        # optional transformations
+        if self.transform:
+            img = self.transform(img)
+        if self.target_transform:
+            label_img = self.target_transform(label_img)
+            label_instance_img = self.target_transform(label_instance_img)
+
+        label_binary = np.zeros(
+            [label_img.shape[0], label_img.shape[1]], dtype=np.uint8
+        )
+        mask = np.where((label_img[:, :, :] != [0, 0, 0]).all(axis=2))
+        label_binary[mask] = 1
+
+        # we could split the instance label here, each instance in one channel (basically a binary mask for each)
+        return img, label_binary, label_instance_img
+
+
+def plot_images(image, mask, mask_instance=None):
+    image_np = image.numpy().transpose(1, 2, 0)
     mask_np = mask.numpy().squeeze()
 
-    # Plot the image and mask
-    fig, axes = plt.subplots(1, 2, figsize=(10, 5))
+    if mask_instance is not None:
+        mask_instance_np = mask_instance.numpy().squeeze()
+        fig, axes = plt.subplots(1, 3, figsize=(15, 5))
+        axes[2].imshow(mask_instance_np, cmap="gray")
+        axes[2].set_title("Mask Instance")
+        axes[2].axis("off")
+    else:
+        fig, axes = plt.subplots(1, 2, figsize=(10, 5))
 
     axes[0].imshow(image_np)
     axes[0].set_title("Image")
@@ -109,3 +164,123 @@ if __name__ == "__main__":
     axes[1].axis("off")
 
     plt.show()
+
+
+if __name__ == "__main__":
+    config = read_yaml(CONFIG_PATH)
+    example_dataset = ["Tusimple", "Maskbdd100k"]
+    dataset = example_dataset[0]
+
+    resize_height = int(config["main"]["resize_height"])
+    resize_width = int(config["main"]["resize_width"])
+
+    if dataset == "Tusimple":
+
+        data_transforms = {
+            "train": transforms.Compose(
+                [
+                    transforms.Resize((resize_height, resize_width)),
+                    transforms.ColorJitter(
+                        brightness=0.1, contrast=0.1, saturation=0.1, hue=0.1
+                    ),
+                    transforms.ToTensor(),
+                    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+                ]
+            ),
+            "val": transforms.Compose(
+                [
+                    transforms.Resize((resize_height, resize_width)),
+                    transforms.ToTensor(),
+                    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+                ]
+            ),
+        }
+
+        target_transforms = transforms.Compose(
+            [
+                Rescale((resize_width, resize_height)),
+            ]
+        )
+
+        train_dataset_file = os.path.join(
+            config["dataset"]["tusimple"]["train"]["dir"], "train.txt"
+        )
+        val_dataset_path = os.path.join(
+            config["dataset"]["tusimple"]["train"]["dir"], "val.txt"
+        )
+
+        train_dataset = TusimpleSet(
+            train_dataset_file,
+            transform=data_transforms["train"],
+            target_transform=target_transforms,
+        )
+        train_loader = DataLoader(
+            train_dataset, batch_size=config["dataloader"]["batch_size"], shuffle=True
+        )
+
+        data_iter = iter(train_loader)
+        img, label_binary, label_instance_img = next(data_iter)
+
+        image = img[0]
+        mask = label_binary[0]
+        mask_instance = label_instance_img[0]
+
+        print("Image Tensor Type:", type(image))
+        print("Image Tensor Shape:", image.shape)
+        print("Mask Tensor Type:", type(label_binary))
+        print("Mask Tensor Shape:", mask.shape)
+        print("Mask Instance Tensor Type:", type(label_instance_img))
+        print("Mask Instance Tensor Shape:", mask_instance.shape)
+
+        plot_images(img[0], label_binary[0], label_instance_img[0])
+
+    elif dataset == "Maskbdd100k":
+
+        data_transforms = {
+            "train": transforms.Compose(
+                [
+                    transforms.Resize((resize_height, resize_width)),
+                    # transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1, hue=0.1),
+                    transforms.ToTensor(),
+                    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+                ]
+            ),
+            "val": transforms.Compose(
+                [
+                    transforms.Resize((resize_height, resize_width)),
+                    transforms.ToTensor(),
+                    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+                ]
+            ),
+        }
+
+        target_transforms = transforms.Compose(
+            [
+                transforms.Resize((resize_height, resize_width)),
+                transforms.ToTensor(),
+            ]
+        )
+
+        dataset = MaskDataset(
+            images_path=config["dataset"]["bdd100k"]["train"]["images_path"],
+            mask_path=config["dataset"]["bdd100k"]["train"]["labels_path"],
+            transform=data_transforms["train"],
+            target_transform=target_transforms,
+        )
+        dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
+
+        data_iter = iter(dataloader)
+        images, masks = next(data_iter)
+
+        image = images[0]
+        mask = masks[0]
+
+        print("Image Tensor Type:", type(image))
+        print("Image Tensor Shape:", image.shape)
+
+        print("Mask Tensor Type:", type(mask))
+        print("Mask Tensor Shape:", mask.shape)
+
+        plot_images(image, mask)
+    else:
+        print("Select good dataset name")
